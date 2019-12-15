cfaa5c024d0226554ee00cae597086ad1ec24ed3
NoFocuss <timothydtracy@gmail.com>
2019-12-14 21:40:32 -0800

    test

--- cgi-enabled/test.py
+++ cgi-enabled/test.py
@@ -9,16 +9,16 @@ sys.path.insert(0, "/home/ubuntu/nofocus-tk/cgi-enabled")
 import elem
 print("Content-Type: text/html")
 print("")
-
+n = ""
 head = []
 
 head.append("<link rel=\"stylesheet\" type=\"text/css\" href=\"./../style.css\">")
-head.append(elem.createElem("title", "none", "test.py"))
+head.append(elem.createElem("title", None, "test.py"))
 
 content = ""
 for element in head:
 	content+=element
-print(elem.createElem("head", "none", content))
-print(elem.openElem("form", "none"))
-print(elem.openElem("input", "id=\"username\"type=\"text\""))
+print(elem.createElem("head", None, content))
+print(elem.openElem("form"))
+print(elem.openElem("input", (f"{attr("id", "username")} {attr("type", "text")}"))
 print(elem.closeElem("form"))
